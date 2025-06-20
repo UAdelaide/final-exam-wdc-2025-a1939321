56e37b8386d93132e7c204aa6e7f85fd056f3d0d
Nhut Anh Tran <trany050@mymail.unisa.edu.au>
2025-06-20 13:27:34 +0000

    [automatic commit] 2025-06-20 13:27:34 UTC ;; communications error to 208.67.222.222#53: timed out 122.150.236.98

--- part1/routes/dogwalkservice.js
+++ part1/routes/dogwalkservice.js
@@ -14,7 +14,9 @@ router.get('/dogs', async function(req, res) {
 // GET open walkrequests
 router.get('/walkrequests', async function(req, res) {
     const [rows] = await db.query(`
-        SELECT * FROM WalkRequests WHERE status = "open"`);
+        SELECT
+        FROM WalkRequests w
+        WHERE status = "open"`);
     res.json(rows);
 });
 
