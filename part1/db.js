6e1d68c44402c0dab1088c01a877546d1db6445d
Nhut Anh Tran <trany050@mymail.unisa.edu.au>
2025-06-20 13:21:54 +0000

    [automatic commit] 2025-06-20 13:21:54 UTC 122.150.236.218

--- part1/db.js
+++ part1/db.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2/promise');
 
-const db =mysql.createPool({
+const db = mysql.createPool({
     //socketPath: '/var/run/mysqld/mysqld.sock',
     host: '127.0.0.1',
     user: 'root',
