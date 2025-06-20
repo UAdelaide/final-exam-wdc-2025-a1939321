e01763a810a05c453dcb0542de6d53f4aece32c1
Nhut Anh Tran <trany050@mymail.unisa.edu.au>
2025-06-20 12:54:04 +0000

    [automatic commit] 2025-06-20 12:54:04 UTC 122.150.236.152

--- part1/db.js
+++ part1/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const db =mysql.createPool({
-    host: 'localhost,
+    host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'DogWalkService',
