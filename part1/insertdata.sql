b2115aea19647054bf6680cb5ee651e4e97be691
Nhut Anh Tran <trany050@mymail.unisa.edu.au>
2025-06-20 12:30:24 +0000

    [automatic commit] 2025-06-20 12:30:24 UTC 122.150.236.218

--- part1/insertdata.sql
+++ part1/insertdata.sql
@@ -3,7 +3,7 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
     ('alice123', 'alice@example.com', 'hashed123', 'owner'),
     ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
     ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-    ('david456', 'david@example.com', 'hashed101', 'walker'),
+    ('huyle', 'david@example.com', 'hashed101', 'walker'),
     ('emma789', 'emma@example.com', 'hashed202', 'owner');
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
@@ -13,7 +13,6 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
     ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Charlie', 'medium'),
     ((SELECT user_id FROM Users WHERE username = 'emma789'), 'Daisy', 'small');
 
--- Insert five walk requests into the WalkRequests table, using subqueries to get dog_id
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
     ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
     ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
