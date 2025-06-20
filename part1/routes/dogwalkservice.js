a904f595b282457e7f61e6475465373fb2fc2b1f
Nhut Anh Tran <trany050@mymail.unisa.edu.au>
2025-06-20 13:34:21 +0000

    [automatic commit] 2025-06-20 13:34:21 UTC 122.150.236.218

--- part1/routes/dogwalkservice.js
+++ part1/routes/dogwalkservice.js
@@ -24,7 +24,7 @@ router.get('/walkrequests', async function(req, res) {
 // GET Walker SUMMARY
 router.get('walker/summary', async function(req, res) {
     const [rows] = await db.query(`
-        SELECT 
+        SELECT
         FROM USers u`);
     res.json(rows);
 });
