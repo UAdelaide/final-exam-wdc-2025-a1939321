adbd340c9545ca245e4a3fc53d36451d4ec20e93
Nhut Anh Tran <trany050@mymail.unisa.edu.au>
2025-06-21 06:56:05 +0000

    [automatic commit] 2025-06-21 06:56:05 UTC 122.150.236.30

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -64,6 +64,7 @@ router.post('/logout', function (req, res) {
   });
 
 router.get('/dog', async function(req, res) {
-  const [rows] = await db.query(``))
+  const [rows] = await db.query(`
+    SELECT  d.dog_id, dname,`))
 
 module.exports = router;
