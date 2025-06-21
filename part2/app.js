5952a65846a094cfeb31403a07502182b9185d45
Nhut Anh Tran <trany050@mymail.unisa.edu.au>
2025-06-21 02:42:15 +0000

    [automatic commit] 2025-06-21 02:42:15 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out ;; no servers could be reached

--- part2/app.js
+++ part2/app.js
@@ -7,20 +7,19 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(cookieParser());
+
+// Routes
+const walkRoutes = require('./routes/walkRoutes');
+const userRoutes = require('./routes/userRoutes');
+
 app.use(session({
     secret: 'b706835de79a2b4e80506f582af3676ac8361638',
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
 }));
-
-app.use(express.static(path.join(__dirname, '/public')));
-
-// Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
-
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
