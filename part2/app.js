53b9cf9c68da92ecf4c213fbebaf8ca71aafe612
Nhut Anh Tran <trany050@mymail.unisa.edu.au>
2025-06-21 01:39:16 +0000

    [automatic commit] 2025-06-21 01:39:16 UTC 122.150.236.30

--- part2/app.js
+++ part2/app.js
@@ -2,12 +2,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const mysql = require('mysql');
 
 const app = express();
-
-);
-});
 
 // Middleware
 app.use(express.json());
