2969219f44cd8cfdc1aa240e66a69aa5ce2657c6
Nhut Anh Tran <trany050@mymail.unisa.edu.au>
2025-06-21 00:58:42 +0000

    [automatic commit] 2025-06-21 00:58:42 UTC 122.150.236.218

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -188,7 +188,7 @@ function login(){
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             const data = JSON.parse(this.responseText);
-            if
+            if (data.user.role === 'owner')
         } else if (this.readyState == 4 && this.status >= 400) {
             alert("Login failed");
         }
