3557623cb8e4a32d458d090d6ec99b8e96c4a89f
Nhut Anh Tran <trany050@mymail.unisa.edu.au>
2025-06-21 01:38:31 +0000

    [automatic commit] 2025-06-21 01:38:30 UTC 122.150.236.152

--- part2/app.js
+++ part2/app.js
@@ -17,6 +17,7 @@ app.use(function(req, res, next) {
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
     secret: 'b706835de79a2b4e80506f582af3676ac8361638',
